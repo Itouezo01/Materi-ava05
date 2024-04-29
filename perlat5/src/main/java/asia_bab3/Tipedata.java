1e9b20ded845297e7f44aea32300b26a564d993f
A-16 <A-16@A-16>
2024-04-29 10:32:43 +0700

    Bab 3

---
+++ perlat5/src/main/java/asia_bab3/Tipedata.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asia_bab3;
+
+/**
+ *
+ * @author A-16
+ */
+public class Tipedata {
+    
+}
--- perlat5/src/main/java/com/mycompany/perlat5/Lat5.java
+++ perlat5/src/main/java/com/mycompany/perlat5/Lat5.java
@@ -1,17 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.perlat5;
+
+import java.util.Scanner;
 
 /**
  *
  * @author A-16
  */
 public class Lat5 {
+
     public static void main(String[] args) {
-        System.out.println("Hallo");
-        
+// Deklarasi variabel
+        char huruf;
+        boolean isMasihBelajar;
+        int nilaiUjian;
+        double hargaBarang;
+        float diskon;
+        byte umur;
+        short kodePos;
+        long populasiNegara;
+// Membuat objek Scanner untuk input data
+        Scanner scanner = new Scanner(System.in);
+// Input data karakter
+        System.out.println("Masukkan huruf: ");
+        huruf = scanner.next().charAt(0);
+// Input data boolean
+        System.out.println("Apakah Anda masih belajar (true/false): ");
+        isMasihBelajar = scanner.nextBoolean();
+// Input data numerik (int)
+        System.out.println("Masukkan nilai ujian (int): ");
+        nilaiUjian = scanner.nextInt();
+// Input data numerik (double)
+        System.out.println("Masukkan harga barang (double): ");
+        hargaBarang = scanner.nextDouble();
+// Input data numerik (float)
+        System.out.println("Masukkan diskon (float): ");
+        diskon = scanner.nextFloat();
+// Input data numerik (byte)
+        System.out.println("Masukkan umur (byte): ");
+        umur = scanner.nextByte();
+// Input data numerik (short)
+        System.out.println("Masukkan kode pos (short): ");
+        kodePos = scanner.nextShort();
+// Input data numerik (long)
+        System.out.println("Masukkan populasi negara (long): ");
+        populasiNegara = scanner.nextLong();
+// Menampilkan data yang diinput
+        System.out.println("\nData yang Anda masukkan:");
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Masih belajar: " + isMasihBelajar);
+        System.out.println("Nilai ujian: " + nilaiUjian);
+        System.out.println("Harga barang: " + hargaBarang);
+        System.out.println("Diskon: " + diskon);
+        System.out.println("Umur: " + umur);
+        System.out.println("Kode pos: " + kodePos);
+        System.out.println("Populasi negara: " + populasiNegara);
     }
 }
